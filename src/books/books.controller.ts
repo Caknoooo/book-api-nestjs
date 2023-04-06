a8427955934d717a1e7b1910201f8639c3ad6759
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-04-06 21:26:00 +0700

    feat: add books service

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { BooksService } from './books.service';
 
 @Controller('books')
@@ -17,6 +25,11 @@ export class BooksController {
     return this.booksService.getAllBooks();
   }
 
+  @Get('/:id')
+  getBookById(@Param('id') id: string) {
+    return this.booksService.getBookById(id);
+  }
+
   @Post()
   createBook(
     @Body('title') title: string,
@@ -24,5 +37,21 @@ export class BooksController {
     @Body('category') category: string,
   ) {
     return this.booksService.createBook(title, author, category);
+  }
+
+  @Put('/:id')
+  updateBook(
+    @Param('id') id: string,
+    @Body('title') title: string,
+    @Body('author') author: string,
+    @Body('category') category: string,
+  ) {
+    console.log(id, title, author, category);
+    return this.booksService.updateBook(id, title, author, category);
+  }
+
+  @Delete('/:id')
+  deleteBook(@Param('id') id: string) {
+    this.booksService.deleteBook(id);
   }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
@@ -9,6 +9,11 @@ export class BooksService {
     return this.books;
   }
 
+  getBookById(id: string) {
+    const bookIdx = this.findBookById(id);
+    return this.books[bookIdx];
+  }
+
   createBook(title: string, author: string, category: string) {
     this.books.push({
       id: uuidv4(),
@@ -16,5 +21,26 @@ export class BooksService {
       author,
       category,
     });
+  }
+
+  updateBook(id: string, title: string, author: string, category: string) {
+    const bookIdx = this.findBookById(id);
+    this.books[bookIdx].title = title;
+    this.books[bookIdx].author = author;
+    this.books[bookIdx].category = category;
+    console.log(this.books[bookIdx]);
+  }
+
+  deleteBook(id: string) {
+    const bookIdx = this.findBookById(id);
+    this.books.splice(bookIdx, 1);
+  }
+
+  findBookById(id: string) {
+    const bookIdx = this.books.findIndex((book) => book.id === id);
+    if (bookIdx === -1) {
+      throw new NotFoundException(`Book with id ${id} is not found`);
+    }
+    return bookIdx;
   }
 }
